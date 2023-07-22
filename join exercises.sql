29ef97ae3c8e0e09e38c7830d8eccf8ae2a1e32f
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-22 10:36:28 -0500

    Fixed error and completed additional exercise

--- join exercises.sql
+++ join exercises.sql
@@ -30,14 +30,14 @@ WHERE categories.Name = "Appliances" OR categories.Name = "Games";
 SELECT products.Name, sum(sales.Quantity) as "Total Units sold", sum(sales.PricePerUnit * sales.Quantity) as "Total price sold"
 FROM products
 INNER JOIN sales
-ON products.productID = sales.productID
-WHERE products.Name LIKE "%hotel%california%";
+ON sales.productID = products.productID
+WHERE products.ProductID = 97;
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
 SELECT products.Name, reviews.Reviewer, reviews.Comment
 FROM products
 INNER JOIN reviews
 ON products.ProductID = reviews.ProductID
-WHERE products.Name LIKE "%Visio%"
+WHERE products.Name LIKE "%Visio%";
 
 -- ------------------------------------------ Extra - May be difficult
 /* Your goal is to write a query that serves as an employee sales report.
@@ -46,3 +46,11 @@ This query should return:
 -  the employee's first and last name
 -  the name of each product
 -  and how many of that product they sold */
+SELECT employees.EmployeeID, employees.FirstName, employees.LastName, products.Name, Sum(sales.Quantity) as TotalSold
+FROM sales
+INNER JOIN employees on employees.EmployeeID = sales.EmployeeID
+INNER JOIN products on products.ProductID = sales.ProductID
+GROUP BY employees.EmployeeID, products.ProductID
+ORDER BY employees.FirstName;
+
+
